402c12d83ed95e0abb5aa19c9829b1bf0b8dd462
labietelabiete <victorhugogote@gmail.com>
2021-04-08 17:52:44 +0200

    New Event Modal Completed

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,38 @@
-var modal = document.getElementById("newEventModal");
+var modalNewEvent = document.getElementById("newEventModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("newEventBtn");
+var btnNewEevent = document.getElementById("newEventBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-var cancel = document.getElementById("cancel");
+var cancel = document.getElementById("cancelNewEvent");
 
 
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btnNewEevent.onclick = function() {
+  modalNewEvent.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modalNewEvent.style.display = "none";
 }
 
 cancel.onclick = function() {
-  modal.style.display = "none";
+  modalNewEvent.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalNewEvent) {
+    modalNewEvent.style.display = "none";
   }
 }
 
 document.onkeydown = function(event) {
   event = event || window.event;
   if (event.keyCode == 27) {
-    modal.style.display = "none";
+    modalNewEvent.style.display = "none";
   }
 };
