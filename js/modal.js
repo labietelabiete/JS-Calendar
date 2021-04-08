8503a9ce7c85afba55802611882d724500620eb2
labietelabiete <victorhugogote@gmail.com>
2021-04-08 19:27:59 +0200

    Check Event Modal 100%

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,66 @@
-var modalNewEvent = document.getElementById("newEventModal");
+let modalNewEvent = document.getElementById("newEventModal");
+let modalCheckEvent = document.getElementById("checkEventModal");
 
 // Get the button that opens the modal
-var btnNewEevent = document.getElementById("newEventBtn");
+let btnNewEvent = document.getElementById("newEventBtn");
+let btnCheckEvent = document.getElementById("checkEventBtn");
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let closeModal = document.getElementsByClassName("close");
 
-var cancel = document.getElementById("cancelNewEvent");
+let cancelNewEvent = document.getElementById("cancelNewEvent");
+let removeEvent = document.getElementById("RemoveEventButton");
+let okEvent = document.getElementById("okCheckEventButton"); 
 
 
 
 // When the user clicks the button, open the modal 
-btnNewEevent.onclick = function() {
+btnNewEvent.onclick = function() {
   modalNewEvent.style.display = "block";
 }
+btnCheckEvent.onclick = function() {
+  modalCheckEvent.style.display = "block";
+}
+
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeModal[0].onclick = function() {
+  modalNewEvent.style.display = "none";
+}
+closeModal[1].onclick = function() {
+  modalCheckEvent.style.display = "none";
+}
+
+// When the user clicks on cancel, close the modal
+cancelNewEvent.onclick = function() {
   modalNewEvent.style.display = "none";
 }
 
-cancel.onclick = function() {
-  modalNewEvent.style.display = "none";
+// When the user clicks on OK, close the modal
+okEvent.onclick = function() {
+  modalCheckEvent.style.display = "none";
 }
+
+//When the user click on remove event
+removeEvent.onclick = function() {
+  modalCheckEvent.style.display = "none";
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modalNewEvent) {
+  if (event.target == modalCheckEvent || event.target == modalNewEvent) {
     modalNewEvent.style.display = "none";
+    modalCheckEvent.style.display = "none";
   }
 }
+
 
 document.onkeydown = function(event) {
   event = event || window.event;
   if (event.keyCode == 27) {
     modalNewEvent.style.display = "none";
+    modalCheckEvent.style.display = "none";
   }
 };
