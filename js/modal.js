debb9ead80afbc83f659f4d9a1586e6e6351934e
김은영 <solaz0824@gmail.com>
2021-04-08 17:12:09 +0200

    modal commit buttons added

--- js/modal.js
+++ js/modal.js
@@ -6,6 +6,10 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var cancel = document.getElementById("cancel");
+
+
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -16,9 +20,19 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+cancel.onclick = function() {
+  modal.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+document.onkeydown = function(event) {
+  event = event || window.event;
+  if (event.keyCode == 27) {
+    modal.style.display = "none";
+  }
+};
