e977bb304d2a6ecf23e48a441fd63358f74c5612
Ricard Garcia <imricardgarcia@gmail.com>
2021-04-16 14:11:08 +0200

    Changed minimum length of title to display animation to 10

--- js/eventCalendar.js
+++ js/eventCalendar.js
@@ -79,7 +79,7 @@ function createEventDiv(dayID, eventID, eventType, eventTitle){
 
         // Title container (wrapper)
         let newContainerDiv = document.createElement("div");
-        if(eventTitle.length > 20) {
+        if(eventTitle.length > 10) {
             newContainerDiv.setAttribute("class", "titleContainerId"+eventID);
             newContainerDiv.classList.add("titleContainer");
         } else {
